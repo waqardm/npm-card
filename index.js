f545f73e7cf3a2d8b80f7bab97adbd7365d63224
Waqar Mohammad <waqar@waqar.co>
2018-12-20 08:47:20 +0000

    Updated colour error and fixed grammar

--- index.js
+++ index.js
@@ -2,14 +2,14 @@
 const {white, cyan, green,bold, blue} = require("colorette")
 
 // Text + chalk definitions
-var work = white(`${blue('Ionic')}, Developer, Advocate`)
-var twitter = cyan('https://twitter.com/mhartington')
-var github = cyan('https://github.com/mhartington')
-var site = cyan('https://mhartington.io')
-var npx = white('npx mhartington')
+var work = white( '🕵🏻‍ Searching for my first tech role')
+var twitter = cyan('https://twitter.com/lawyerscode')
+var github = cyan('https://github.com/waqardm')
+var site = cyan('https://lawyerscode.co.uk')
+var npx = white('npx waqar')
 
 var newline = '\n'
-var heading = `${white('  Mike Hartington /')} ${cyan('mhartington')}`
+var heading = `${white('  Waqar Mohammad /')} ${cyan('lawyerscode')}`
 var working = `${white(bold('     Work:'))}  ${work}`
 var twittering = `${white(bold('  Twitter:'))}  ${twitter}`
 var githubing = `${white(bold('   GitHub:'))}  ${github}`
